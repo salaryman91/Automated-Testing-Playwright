e610b22f187c3855fc298c5be969c2fd2782c0e0
Ray Kim <salaryman91@naver.com>
2025-02-23 19:36:35 +0900

    feat: 테스트 스크립트 추가

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -3,14 +3,15 @@ import { test, expect, Page } from '@playwright/test';
 test.describe('naver access test', () => {
   test.beforeEach(async ({ page }: { page: Page }) => {
     await page.goto('https://www.naver.com');
+    await page.waitForSelector('#query');
   });
 
-  test('pass_test', async ({ page }: { page: Page }) => {
+  test('test_1', async ({ page }: { page: Page }) => {
     const url = page.url();
     expect(url).toContain('naver');
   });
 
-  test('fail_test', async ({ page }: { page: Page }) => {
+  test('test_2', async ({ page }: { page: Page }) => {
     const url = page.url();
     expect(url).toContain('never');
   });
